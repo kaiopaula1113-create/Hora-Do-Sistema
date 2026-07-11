b07b62284668a9e5e0743d480fc98250ff9860ad
Kaio Nicollas <kaiopaula1113@gmail.com>
2026-07-10 23:32:49 -0300

    Add files via upload

---
+++ HoraDoSistema.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+public class HoraDoSistema{
+    public static void main(String[] args) {
+        Date tempo = new Date();
+        System.out.println("A hora do sistema é: ");
+        System.out.println(tempo.toString());
+    }
+}
