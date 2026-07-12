7751f9ac2a8733ee88e8870c9e70c9f4e37e4406
Kaio Nicollas <kaiopaula1113@gmail.com>
2026-07-12 20:50:46 -0300

    Simplify output of system time

--- HoraDoSistema.java
+++ HoraDoSistema.java
@@ -3,6 +3,6 @@ public class HoraDoSistema{
     public static void main(String[] args) {
         Date tempo = new Date();
         System.out.println("A hora do sistema é: ");
-        System.out.println(tempo.toString());
+        System.out.println(tempo);
     }
 }
